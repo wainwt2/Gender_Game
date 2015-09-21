dc8b25a8e3e0ccd6542e312e4de1b1fb05dc101a
Paul Horner <hornep@rpi.edu>
2015-09-21 16:34:59 -0700

    click and drag script and meta data for it

---
+++ Gender_Game/Assets/Click_and_Drag.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click_and_Drag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		//on mouse down 
+		if (Input.GetMouseButtonDown(0)) {
+			
+			Vector3 MousePosition = Input.mousePosition;//gets mouse position
+			MousePosition.z = Camera.main.transform.position.z;//distance from camera to screen
+			MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);//converts mouse position(pixels) to world position
+			Debug.Log(MousePosition);
+			
+			if(GetComponent<Collider2D>().OverlapPoint(MousePosition)){
+				Debug.Log("clicked on");
+				OnMouseDrag();
+				//transform.position = new Vector3(MousePosition.x, MousePosition.y, 0);//set object position to mouse position while mouse is down
+				//Debug.Log (transform.position);
+				}
+			
+		}
+		
+	}
+
+	void OnMouseDrag(){
+		Vector3 MousePosition = Input.mousePosition;//gets mouse position
+		MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);//converts mouse position(pixels) to world position
+		transform.position = new Vector3(MousePosition.x, MousePosition.y, 0);//set object position to mouse position while mouse is down
+	}
+	
+}
