85c35988036f38a4ae3e2027941ed1e2a1bd5fe5
Tim Huang <huangc8@rpi.edu>
2015-09-23 05:27:03 -0400

    Animation Coding complete, just waiting for the sprites

--- Gender_Game/Assets/Script/Click_and_Drag.cs
+++ Gender_Game/Assets/Script/Click_and_Drag.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Click_and_Drag : MonoBehaviour {
 
+	public bool dragging = false; // whether the object is been drag
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,31 +12,30 @@ public class Click_and_Drag : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		
+
 		//on mouse down 
 		if (Input.GetMouseButtonDown(0)) {
-			
 			Vector3 MousePosition = Input.mousePosition;//gets mouse position
 			MousePosition.z = Camera.main.transform.position.z;//distance from camera to screen
 			MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);//converts mouse position(pixels) to world position
-			Debug.Log(MousePosition);
+			//Debug.Log(MousePosition);
 			
 			if(GetComponent<Collider2D>().OverlapPoint(MousePosition)){
-				Debug.Log("clicked on");
+				//Debug.Log("clicked on");
 				OnMouseDrag();
 				//transform.position = new Vector3(MousePosition.x, MousePosition.y, 0);//set object position to mouse position while mouse is down
 				//Debug.Log (transform.position);
-				}
-			
+			}
+		}else{
+			dragging = false;
 		}
-		
 	}
 
 	void OnMouseDrag(){
 		Vector3 MousePosition = Input.mousePosition;//gets mouse position
 		MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);//converts mouse position(pixels) to world position
 		transform.position = new Vector3(MousePosition.x, MousePosition.y, 0);//set object position to mouse position while mouse is down
+		dragging = true;
 	}
 	
 }
---
+++ Gender_Game/Assets/Script/Creature_Animation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature_Animation : MonoBehaviour {
+
+	private Click_and_Drag _CD;
+	private Animator anim;
+	int DragHash = Animator.StringToHash("Drag");
+	int RunHash = Animator.StringToHash("Run");
+	int HappyHash = Animator.StringToHash("Happy");
+	bool DragPara = true;
+	bool RunPara = false;
+	bool HappyPara = false;
+
+	// Use this for initialization
+	void Start () {
+		_CD = GetComponent<Click_and_Drag> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// check if being drag
+		DragPara = _CD.dragging;
+
+		// if not being drag
+		if (!DragPara) {
+			// check if not happy 
+			if(!HappyPara){
+				RunPara = true;	
+			}else{
+				RunPara = false;
+			}
+		}
+
+		// update animation
+		anim.SetBool ("Drag", DragPara);
+		anim.SetBool ("Run", RunPara);
+		anim.SetBool ("Happy", HappyPara);
+	}
+
+	public void IsHappy(bool happy){
+		HappyPara = happy;
+	}
+}
